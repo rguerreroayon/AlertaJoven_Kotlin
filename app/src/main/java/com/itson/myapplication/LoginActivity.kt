0f2587f4a89448dcb4061eabf493a840ba83b7d6
Roberto G <rguerreroayon@gmail.com>
2019-05-19 00:19:27 -0700

    no jala el google login :('

--- app/src/main/java/com/itson/myapplication/LoginActivity.kt
+++ app/src/main/java/com/itson/myapplication/LoginActivity.kt
@@ -1,51 +1,77 @@
 package com.itson.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.CardView
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 
 class LoginActivity : AppCompatActivity() {
 
-    //TextViews
-    private var mEmailTextView: TextView? = null
-
-    //Buttons
-    private var mLoginButton: Button? = null
-    private var mRegisterButton: Button? = null
-    private var mFacebookButton: Button? = null
-    private var mGoogleButton: SignInButton? = null
-
-    //EditTexts
-    private var mEmailEditText: EditText? = null
-    private var mUserNameEditText: EditText? = null
-    private var mUserPassword: EditText? = null
-
-    //CardView
-    private var mCredentialsCardView: CardView? = null
-    private var mInvalidLoginPopup: CardView? = null
-
-    private var mFirebaseAuth: FirebaseAuth? = null
-
-
-    //https://alertajovenkotlin.firebaseapp.com/__/auth/handler
-
-    private val mOriginalConstraintSet = ConstraintSet()
+    lateinit var mGoogleSignInClient : GoogleSignInClient
+    lateinit var gso: GoogleSignInOptions
+    val RC_SIGN_IN : Int = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+        val signIn = findViewById<View>(R.id.login_google) as SignInButton
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
 
-        mGoogleButton = findViewById(R.id.google_login)
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
+
+
+        signIn.setOnClickListener{
+            view: View? -> signInGoogle()
+        }
+
+
 
 
 
     }
+
+    private fun signInGoogle(){
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == RC_SIGN_IN){
+            val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account : GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+        }catch (e:ApiException){
+            Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount){
+        Toast.makeText(this,account.displayName.toString(),Toast.LENGTH_LONG).show()
+    }
+
 }
